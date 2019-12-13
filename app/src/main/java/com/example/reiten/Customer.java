4f47bc48317a29197ad3e3b4ac216322de7e385c
Garvit Agarwal <garvitagarwal984@gmail.com>
2019-12-13 17:39:49 +0530

    Some

--- app/src/main/java/com/example/reiten/Customer.java
+++ app/src/main/java/com/example/reiten/Customer.java
@@ -1,7 +1,5 @@
 package com.example.reiten;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
@@ -10,26 +8,58 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
-public class Customer extends AppCompatActivity {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Customer extends AppCompatActivity {
+    public static final String TAG = "TAG";
     private static final int REQUEST_WRITE_PERMISSION = 786;
     ImageButton pickimage;
     ImageView imageview;
+    String userID;
+    static int PReqCode = 1;
+    static int RequesCode = 1;
+    Uri pickedImgUri;
+    private FirebaseAuth mAuth;
+    FirebaseFirestore fStore;
+    private EditText mname, mbhawan;
+    private ProgressBar loadingProgress;
+    private Button submit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer);
         imageview = findViewById(R.id.imageview);
         pickimage = findViewById(R.id.pickimage);
+        fStore = FirebaseFirestore.getInstance();
         pickimage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -43,11 +73,89 @@ public class Customer extends AppCompatActivity {
                 }
             }
         });
+        mname = findViewById(R.id.editText12);
+        mbhawan = findViewById(R.id.editText33);
+        loadingProgress = findViewById(R.id.progressBar2);
+        submit = findViewById(R.id.button37);
+
+        loadingProgress.setVisibility(View.INVISIBLE);
+        mAuth = FirebaseAuth.getInstance();
+        if (mAuth.getCurrentUser() != null) {
+            submit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    loadingProgress.setVisibility(View.VISIBLE);
+                    final String name = mname.getText().toString();
+                    final String bhawan = mbhawan.getText().toString();
+
+
+                    if (TextUtils.isEmpty(name)) {
+                        mname.setError("Name is Required.");
+                        return;
+                    }
+
+                    if (TextUtils.isEmpty(bhawan)) {
+                        mbhawan.setError("Bhawan name is Required.");
+                        return;
+                    }
+
+                    userID = mAuth.getCurrentUser().getUid();
+                    final FirebaseUser currentUser = mAuth.getCurrentUser();
+                    StorageReference mStorage = FirebaseStorage.getInstance().getReference().child("users_photos_"+userID);
+                    final StorageReference imageFilePath = mStorage.child(pickedImgUri.getLastPathSegment());
+                    imageFilePath.putFile(pickedImgUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                            imageFilePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    UserProfileChangeRequest profileUpdate = new UserProfileChangeRequest.Builder().setDisplayName(name)
+                                            .setPhotoUri(uri).build();
+
+                                    currentUser.updateProfile(profileUpdate).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+
+                                            if (task.isSuccessful()) {
+
+                                            }
+
+
+                                        }
+                                    });
+
+                                }
+                            });
+                        }
+                    });
+                    DocumentReference documentReference = fStore.collection("Customers").document(userID);
+                    Map<String, Object> user = new HashMap<>();
+                    user.put("Name", name);
+                    user.put("Bhawan",bhawan);
+                    documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Log.d(TAG, "onSuccess: user Profile is created for " + userID);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d(TAG, "onFailure: " + e.toString());
+                            loadingProgress.setVisibility(View.GONE);
+                        }
+                    });
+
+                }
+            });
+        }
 
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
         if (requestCode == REQUEST_WRITE_PERMISSION && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             //THIS IS HAPPEN WHEN USER CLICK ALLOW ON PERMISSION
             //START PICK IMAGE ACTIVITY
@@ -73,7 +181,7 @@ public class Customer extends AppCompatActivity {
                     //REQUEST COMPRESS SIZE
                     .setRequestedSize(800, 800)
                     //ASPECT RATIO, DELETE IF YOU NEED CROP ANY SIZE
-                    .setAspectRatio(1,1)
+                    .setAspectRatio(1, 1)
                     .start(this);
         }
 
@@ -83,7 +191,8 @@ public class Customer extends AppCompatActivity {
                 Log.i("RESPONSE getUri", result.getUri().toString());
 
                 //GET CROPPED IMAGE URI AND PASS TO IMAGEVIEW
-                imageview.setImageURI(result.getUri());
+                pickedImgUri=result.getUri();
+                imageview.setImageURI(pickedImgUri);
             }
         }
     }
